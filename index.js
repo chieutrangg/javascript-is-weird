df84e6e9bc13cfb38e39c70f5444422ceaa073da
chieutrangg <chieutrangg@gmail.com>
2021-10-27 21:42:05 +0300

    javascript is weird

---
+++ index.js
@@ -0,0 +1,10 @@
+let result = true + false;
+console.log(result);
+/*  Number(true)  // 1
+    Number(false) // 0 
+    1 + 0 = 1
+*/  
+
+let arrayLength = [,,,].length;
+console.log(arrayLength);
+/*[,,,].length is 3. The last comma is a terminal comma. */ 
